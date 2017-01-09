31ba25366da2443e4d9336645d4f914acf0fafcd
xeno6696 <matt.seil@owasp.org>
2017-01-09 20:18:38 +0000

    Completed millerRabin function.

--- primes.py
+++ primes.py
@@ -1,7 +1,7 @@
 import math, binet, random
 
 def isPrime( p ):
-    print fermatTest(p)
+    return fermatTest(p) and not millerRabin(p)
 
 #Heuristic test that returns if a number is "probably prime."
 def fermatTest( p ):
@@ -35,29 +35,38 @@ def millerRabin( p ):
     isComposite = False
 
     if p == 2:
+        return False
+    elif p % 2 == 0: 
         return True
-    elif p % 2 == 0 or p == 1:
+    elif p == 0 or p == 1 or p == 3:
         return False
 
     #Formula requires the expression 2s*d where it represents factoring
     #all powers of 2 from n - 1
     r = p - 1
     s = 0
+    positives = 0
+    negatives = 0
+    #Also could use divmod... returns a quotient and remainder...
     while r % 2 == 0:
         s += 1
-        print "Factoring 2s from %d" % r
+        #print "Factoring 2s from %d" % r
         r /= 2
     
-    for i in xrange(p):
-        a = random.randint(2, p-2)
-        y = pow(a, r) % p
-        if y != 1 and y != -1:
-            j = 1
-            while j <= s and y != p:
-                z = pow(y, 2) % p
-                if z == 1:
-                    return True
-                j += 1
-                if z != p-1:
-                    return True
+    assert(2**s * r == p-1)
+    isComposite = try_composite(p, r)
+    
     return isComposite
+
+def try_composite(p, r):
+    for i in xrange(p):
+        #print "p is {0}".format(p)
+        a = random.randint(2, p)
+        y = pow(a, r, p)
+        if y == 1:
+            return False
+        if pow(p, 2**i * r, p) == p-1:
+            return False
+    
+    return True
+    
