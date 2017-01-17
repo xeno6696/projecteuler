920e58d8328f6c1cddbd0ac1f1bd53628234be25
xeno6696 <matt.seil@owasp.org>
2017-01-17 21:24:55 +0000

    Need to fix the statistical abnormality for generating primes.

--- primes.py
+++ primes.py
@@ -21,7 +21,7 @@ def fermatTest( p, times):
     for i in xrange(times):
         answer = random.randint(1, p - 1)
         raised = pow(answer, p-1) % p
-        #print "pow({0}, {1}-1) % {2} == {3}".format(answer, p, p, raised)
+        print "pow({0}, {1}-1) % {2} == {3}".format(answer, p, p, raised)
         
         if raised == 1:
             positives +=1
@@ -53,7 +53,7 @@ def millerRabin( p, times ):
 #        print "Factoring 2 from {0}".format(r)
         r /= 2
 
-    #print "asserting 2**{0} * {1} = {2} and {3}-1={4}".format(s, r, 2**s *r, p, p-1)
+    print "asserting 2**{0} * {1} = {2} and {3}-1={4}".format(s, r, 2**s *r, p, p-1)
     assert(2**s * r == p-1)
     isComposite = try_composite(p, r, times)
     
@@ -69,11 +69,40 @@ def try_composite(p, r, times):
         a = random.randint(2, p)
         
         y = pow(a, r, p)
-        #print "computing:  {0}**{1} % {2} == {3}".format(a, r, p, y)
+        print "computing:  {0}**{1} % {2} == {3}".format(a, r, p, y)
         if y == 1:            
             return False
         if y != p-1:
             return False
     
     return True
+
+#Unceremoniously copied from WikiPedia.      
+def trialDivision( n ):
+#Return a list of the prime factors for a natural number.
+    if n < 2:
+        return []
+    prime_factors = []
+    for p in prime_sieve(int(n**0.5)):
+        if p*p > n: break
+        while n % p == 0:
+            prime_factors.append(p)
+            n //= p
+    if n > 1:
+        prime_factors.append(n)
+    return prime_factors
+
+def prime_sieve( n ):
+    return mySieve( n )
+
+def mySieve( n ):
+    rval = []
+    for i in range( int(math.sqrt( n ))):
+        if isPrime( i, 10 ):
+            rval.append(i)
+    return rval
+
+def eratostothenes( n ):
+    rval = []
     
+    return rval
--- primesTest.py
+++ primesTest.py
@@ -1,6 +1,7 @@
 import primes
+from collections import Counter
 
-knownPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541 ]
+knownPrimes = [1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541 ]
 
 #for i in xrange(24):
     #print "Fermat: {0}, result: {1}".format(i, primes.fermatTest(i))
@@ -8,21 +9,29 @@ knownPrimes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 6
     #print "isPrime: {0}, result: {1}".format(i, primes.isPrime(i))
     #print "\n"
 
-assert primes.isPrime(1) == True
-assert primes.isPrime(2) == True
-assert primes.isPrime(3) == True
-assert primes.isPrime(4) == False
-assert primes.fermatTest(5) == True
-assert primes.millerRabin(5) == False
-assert primes.isPrime(5) == True
-assert primes.isPrime(6) == False
-assert primes.isPrime(7) == True
-assert primes.isPrime(8) == False
-assert primes.isPrime(11) == True
-assert primes.isPrime(13) == True
-assert primes.isPrime(17) == True
-assert primes.isPrime(19) == True
-assert primes.isPrime(23) == True
+assert primes.isPrime(1, 5) == True
+assert primes.isPrime(2, 5) == True
+assert primes.isPrime(3, 5) == True
+assert primes.isPrime(4, 5) == False
+assert primes.fermatTest(5, 5) == True
+assert primes.millerRabin(5, 5) == False
+assert primes.isPrime(5, 5) == True
+assert primes.isPrime(6, 5) == False
+assert primes.isPrime(7, 5) == True
+assert primes.isPrime(8, 5) == False
+assert primes.isPrime(11, 5) == True
+assert primes.isPrime(13, 5) == True
+assert primes.isPrime(17, 5) == True
+assert primes.isPrime(19, 5) == True
+assert primes.isPrime(23, 5) == True
 
 for i in knownPrimes:
-    assert primes.isPrime(i) == True
+    assert primes.isPrime(i, 10) == True
+
+comparelist = primes.mySieve( 300000 ) 
+
+print Counter(knownPrimes) == Counter(comparelist)
+print knownPrimes
+print comparelist
+
+
